c3a96361f9e11bb5be238f92bae97bb86ab9d4af
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-23 22:16:04 +0200

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Eirik Andresen on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
